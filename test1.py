6fd226e78431e715be20c593630ee58f7715e631
bian <bian@test>
2019-01-22 14:29:18 +0800

    s

--- test1.py
+++ test1.py
@@ -3,6 +3,16 @@ print("易居中国创新研发中心123456")
 print("fdafagewgweggaf43tb")
 print("fdafagewgweggaf43tb")
 print("fdafagewgweggaf43tb")
+print("易居中国创新研发中心123456")
+
+print("fdafagewgweggaf43tb")
+print("fdafagewgweggaf43tb")
+print("fdafagewgweggaf43tb")
+print("易居中国创新研发中心123456")
+
+print("fdafagewgweggaf43tb")
+print("fdafagewgweggaf43tb")
+print("fdafagewgweggaf43tb")
 
 
 print("fdafagewgweggaf43tb213445")
